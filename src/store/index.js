790298d6fa9962930cfbfb418ab489653050d8c4
itsahmadworld <104201963+itsahmadworld@users.noreply.github.com>
2024-03-22 15:43:38 +0500

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+import * as todos from './modules/todos'
+
+
+export default createStore({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    },
+    modules: {
+        todos
+    }
+})
